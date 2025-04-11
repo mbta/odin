f9fbe4d50279fd2d7bb4ca2d7f9e2badd68f5f0a
Ryan Rymarczyk <rymarczy@gmail.com>
2025-04-11 15:00:38 -0400

    metadata limit_k_sorted

--- tests/utils/parquet_test.py
+++ tests/utils/parquet_test.py
@@ -1,11 +1,13 @@
 import os
+import shutil
 import pytest
-from typing import List
+from typing import Generator
 from pathlib import Path
 
 import polars as pl
 import pyarrow.dataset as pd
 import pyarrow.compute as pc
+import pyarrow.parquet as pq
 
 from odin.utils.parquet import pq_rows_and_bytes
 from odin.utils.parquet import pq_rows_per_mb
@@ -15,22 +17,27 @@ from odin.utils.parquet import ds_column_min_max
 from odin.utils.parquet import ds_metadata_min_max
 from odin.utils.parquet import ds_unique_values
 from odin.utils.parquet import _pq_find_part_offset
+from odin.utils.parquet import ds_metadata_limit_k_sorted
+from odin.utils.parquet import pq_path_partitions
+from odin.utils.parquet import row_group_column_stats
+
 
 PQ_NUM_ROWS = 500_000
 PQ_MAX_INT = 50_000
 
 
-@pytest.fixture
-def pq_files(tmp_path) -> List[str]:
+@pytest.fixture(scope="module")
+def pq_files(tmp_path_factory) -> Generator[list[str]]:
     """Create temporary parquet files for testing."""
+    tmp_path = tmp_path_factory.mktemp("pq_files", numbered=False)
     pq_files = []
     pq_files.append(os.path.join(tmp_path, "file=t1", "t1.parquet"))
     os.makedirs(os.path.dirname(pq_files[-1]), exist_ok=True)
     (
         pl.DataFrame()
         .with_columns(
-            pl.int_range(PQ_MAX_INT).sample(PQ_NUM_ROWS, with_replacement=True).alias("row1"),
-            pl.int_range(PQ_MAX_INT).sample(PQ_NUM_ROWS, with_replacement=True).alias("row2"),
+            pl.int_range(PQ_MAX_INT).sample(PQ_NUM_ROWS, with_replacement=True).alias("col1"),
+            pl.int_range(PQ_MAX_INT).sample(PQ_NUM_ROWS, with_replacement=True).alias("col2"),
         )
         .write_parquet(pq_files[-1], row_group_size=int(PQ_NUM_ROWS / 10))
     )
@@ -40,10 +47,10 @@ def pq_files(tmp_path) -> List[str]:
     (
         pl.DataFrame()
         .with_columns(
-            pl.int_range(PQ_MAX_INT).sample(PQ_NUM_ROWS, with_replacement=True).alias("row1"),
-            pl.int_range(PQ_MAX_INT).sample(PQ_NUM_ROWS, with_replacement=True).alias("row2"),
-            pl.int_range(PQ_MAX_INT).sample(PQ_NUM_ROWS, with_replacement=True).alias("row3"),
-            pl.int_range(PQ_MAX_INT).sample(PQ_NUM_ROWS, with_replacement=True).alias("row4"),
+            pl.int_range(PQ_MAX_INT).sample(PQ_NUM_ROWS, with_replacement=True).alias("col1"),
+            pl.int_range(PQ_MAX_INT).sample(PQ_NUM_ROWS, with_replacement=True).alias("col2"),
+            pl.int_range(PQ_MAX_INT).sample(PQ_NUM_ROWS, with_replacement=True).alias("col3"),
+            pl.int_range(PQ_MAX_INT).sample(PQ_NUM_ROWS, with_replacement=True).alias("col4"),
         )
         .write_parquet(pq_files[-1], row_group_size=int(PQ_NUM_ROWS / 10))
     )
@@ -53,18 +60,19 @@ def pq_files(tmp_path) -> List[str]:
     (
         pl.DataFrame()
         .with_columns(
-            pl.int_range(PQ_MAX_INT).sample(PQ_NUM_ROWS, with_replacement=True).alias("row1"),
-            pl.int_range(PQ_MAX_INT).sample(PQ_NUM_ROWS, with_replacement=True).alias("row2"),
-            pl.int_range(PQ_MAX_INT).sample(PQ_NUM_ROWS, with_replacement=True).alias("row3"),
-            pl.int_range(PQ_MAX_INT).sample(PQ_NUM_ROWS, with_replacement=True).alias("row4"),
-            pl.int_range(PQ_MAX_INT).sample(PQ_NUM_ROWS, with_replacement=True).alias("row5"),
-            pl.int_range(PQ_MAX_INT).sample(PQ_NUM_ROWS, with_replacement=True).alias("row6"),
-            pl.lit("single_value").alias("row7"),
+            pl.int_range(PQ_MAX_INT).sample(PQ_NUM_ROWS, with_replacement=True).alias("col1"),
+            pl.int_range(PQ_MAX_INT).sample(PQ_NUM_ROWS, with_replacement=True).alias("col2"),
+            pl.int_range(PQ_MAX_INT).sample(PQ_NUM_ROWS, with_replacement=True).alias("col3"),
+            pl.int_range(PQ_MAX_INT).sample(PQ_NUM_ROWS, with_replacement=True).alias("col4"),
+            pl.int_range(PQ_MAX_INT).sample(PQ_NUM_ROWS, with_replacement=True).alias("col5"),
+            pl.int_range(PQ_MAX_INT).sample(PQ_NUM_ROWS, with_replacement=True).alias("col6"),
+            pl.lit("single_value").alias("col7"),
         )
         .write_parquet(pq_files[-1], row_group_size=int(PQ_NUM_ROWS / 10))
     )
 
-    return pq_files
+    yield pq_files
+    shutil.rmtree(tmp_path)
 
 
 def test_pq_rows_and_bytes(pq_files) -> None:
@@ -106,25 +114,25 @@ def test_ds_column_min_max(pq_files) -> None:
     """Test ds_column_min_max parquet utility."""
     # Keep an eye on this test, the source data is technically random so this could fail...
     ds = ds_from_path(pq_files)
-    assert ds_column_min_max(ds, "row1") == (0, PQ_MAX_INT - 1)
+    assert ds_column_min_max(ds, "col1") == (0, PQ_MAX_INT - 1)
 
     # test partition column
     assert ds_column_min_max(ds, "file") == ("t1", "t3")
 
-    ds_filter = pc.field("row1") < 50
-    assert ds_column_min_max(ds, "row1", ds_filter) == (0, 49)
+    ds_filter = pc.field("col1") < 50
+    assert ds_column_min_max(ds, "col1", ds_filter) == (0, 49)
 
 
 def test_ds_metadata_min_max(pq_files) -> None:
     """Test ds_column_min_max parquet utility."""
     # Keep an eye on this test, the source data is technically random so this could fail...
     ds = ds_from_path(pq_files)
-    assert ds_metadata_min_max(ds, "row1") == (0, PQ_MAX_INT - 1)
+    assert ds_metadata_min_max(ds, "col1") == (0, PQ_MAX_INT - 1)
 
     # test partition column
     assert ds_metadata_min_max(ds, "file") == ("t1", "t3")
 
-    assert ds_metadata_min_max(ds, "row7") == ("single_value", "single_value")
+    assert ds_metadata_min_max(ds, "col7") == ("single_value", "single_value")
 
 
 def test_ds_unique_values(tmp_path) -> None:
@@ -162,3 +170,66 @@ def test__pq_find_part_offset(tmp_path) -> None:
 
     os.remove(file2)
     assert _pq_find_part_offset(tmp_fldr) == 2
+
+
+def test_pq_path_partitions(pq_files) -> None:
+    """Test pq_path_partitions parquet utility."""
+    pq_path = "/test/path/no/partition.parquet"
+    assert pq_path_partitions(pq_path) == []
+
+    pq_path = "/test/path/col1=val1/partition.parquet"
+    assert pq_path_partitions(pq_path) == [("col1", "val1")]
+
+    pq_path = "/test/path/col1=val1/col2=99/partition.parquet"
+    assert pq_path_partitions(pq_path) == [("col1", "val1"), ("col2", "99")]
+
+    assert pq_path_partitions(pq_files[0]) == [("file", "t1")]
+
+
+def test_row_group_column_stats(pq_files) -> None:
+    """Test row_group_column_stats parquet utility."""
+    pq_file = pq.ParquetFile(pq_files[0])
+    pq_metadata = pq_file.metadata
+    for rg_index in range(pq_file.num_row_groups):
+        rg_meta = pq_metadata.row_group(rg_index)
+        with pytest.raises(IndexError):
+            row_group_column_stats(rg_meta, "col7")
+
+        stats = row_group_column_stats(rg_meta, "col1")
+        assert stats["max"] <= PQ_MAX_INT - 1
+        assert stats["min"] >= 0
+
+        stats = row_group_column_stats(rg_meta, "col2")
+        assert stats["max"] <= PQ_MAX_INT - 1
+        assert stats["min"] >= 0
+
+
+def test_ds_metadata_limit_k_sorted(pq_files) -> None:
+    """
+    Test ds_metadata_limit_k_sorted parquet utility.
+
+    The scope of this test is pretty limited and could be expanded...
+    """
+    ds = ds_from_path(pq_files)
+
+    # test column not in all files
+    df = ds_metadata_limit_k_sorted(ds, "col7")
+    assert df.shape == (PQ_NUM_ROWS, 8)
+
+    df = ds_metadata_limit_k_sorted(ds, "col1")
+    assert df.shape == (PQ_NUM_ROWS * 3, 8)
+    assert df.get_column("col1")[0] == 0
+    assert df.get_column("col1")[-1] == PQ_MAX_INT - 1
+
+    df = ds_metadata_limit_k_sorted(ds, "col1", min_sort_value=500)
+    assert df.width == 8
+    assert df.height < PQ_NUM_ROWS * 3
+    assert df.get_column("col1")[0] == 501
+    assert df.get_column("col1")[-1] == PQ_MAX_INT - 1
+
+    df = ds_metadata_limit_k_sorted(
+        ds, "col1", ds_filter=(pc.field("col7") == "single_value"), ds_filter_columns=["col7"]
+    )
+    assert df.shape == (PQ_NUM_ROWS, 8)
+    assert df.get_column("col1")[0] == 0
+    assert df.get_column("col1")[-1] == PQ_MAX_INT - 1
