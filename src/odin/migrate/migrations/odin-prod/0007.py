693e8c2bd51adc9264df5759957a2baf1ca35c89
Ryan Rymarczyk <rymarczy@gmail.com>
2025-07-02 10:38:05 -0400

    chore: Reset S&B AFC API Datasets
    This change adds migrations to reset S&B AFC API files in PROD and DEV.
    
    This reset is required because S&B updated the types on many tables of their API. This new type schema should be final and consistent through the AFC 2025 launch.

---
+++ src/odin/migrate/migrations/odin-prod/0007.py
@@ -0,0 +1,22 @@
+import os
+
+from odin.utils.aws.s3 import list_objects
+from odin.utils.aws.s3 import delete_objects
+from odin.utils.locations import DATA_SPRINGBOARD
+from odin.utils.locations import AFC_DATA
+
+
+def migration() -> None:
+    """
+    ODIN PROD Migration 0007.
+
+    July 2, 2025
+
+    This migration is to reset S&B API files after changes have been deployed
+    to fix type inconsistencies.
+    After deletion, files will be re-created on next ArchiveAFCAPI job runs.
+
+    1. Delete all parquet files from S&B API prefix.
+    """
+    sb_prefix = os.path.join(DATA_SPRINGBOARD, AFC_DATA)
+    delete_objects([obj.path for obj in list_objects(sb_prefix)])
