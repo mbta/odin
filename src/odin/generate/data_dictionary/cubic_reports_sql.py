f2f25c7d7e9de6271e740f754d52bdbd2e0be71e
Ted Alexander <82063375+ealexa05@users.noreply.github.com>
2025-10-07 17:45:40 -0400

    Adding view for AD_HOC_JOURNAL_ENTRIES (#67)

--- src/odin/generate/data_dictionary/cubic_reports_sql.py
+++ src/odin/generate/data_dictionary/cubic_reports_sql.py
@@ -534,3 +534,26 @@ WA160 = """
             on fpuld.fare_prod_users_list_key = fpd.fare_prod_users_list_key
     ;
 """
+
+AD_HOC_JOURNAL_ENTRIES = """
+    DROP VIEW IF EXISTS cubic_reports.ad_hoc_journal_entries;
+    CREATE VIEW cubic_reports.ad_hoc_journal_entries
+    AS
+    SELECT
+    accounting_date,
+    voucher,
+    ledger_account,
+    txn_currency_debit_amount,
+    txn_currency_credit_amount,
+    external_account_name,
+    description,
+    split_part(description, '|', 1) as transaction_category,
+    split_part(description, '|', 2) as apportionment_rule,
+    split_part(description, '|', 4) as operating_date,
+    split_part(description, '|', 5) as fare_instrument_id
+    FROM
+        cubic_ods.edw_fnp_general_jrnl_account_entry
+    WHERE
+        ledger_name = 'MBTA'
+    ;
+"""
