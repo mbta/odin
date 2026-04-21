78b6ee880a8e9d65bbadd004f3ce32f908efaf72
Ted Alexander <82063375+ealexa05@users.noreply.github.com>
2026-04-21 11:28:32 -0400

    Triggering regeneration for EDW.TOKEN_HISTORY (#135)

---
+++ src/odin/migrate/migrations/odin-dev/0011.py
@@ -0,0 +1,40 @@
+import os
+
+from odin.utils.aws.s3 import list_objects
+from odin.utils.aws.s3 import delete_objects
+from odin.utils.locations import DATA_SPRINGBOARD
+from odin.utils.locations import CUBIC_ODS_FACT_DATA
+from odin.utils.logger import ProcessLog
+
+TABLES_TO_DELETE: list[str] = [
+    "EDW.TOKEN_HISTORY",
+]
+
+
+def migration() -> None:
+    """
+    ODIN DEV Migration 0011.
+
+    April 21, 2026
+
+    Delete all files under
+    s3://<springboard>/odin/data/cubic/ods/<table>/
+    for each table in TABLES_TO_DELETE.
+    """
+    log = ProcessLog("odin_migration", migration="dev_0011")
+    failures: dict[str, int] = {}
+
+    for table in TABLES_TO_DELETE:
+        prefix = os.path.join(DATA_SPRINGBOARD, CUBIC_ODS_FACT_DATA, table, "")
+        remaining = delete_objects([obj.path for obj in list_objects(prefix)])
+        if remaining:
+            failures[prefix] = len(remaining)
+
+    log.add_metadata(
+        tables_attempted=len(TABLES_TO_DELETE),
+        tables_failed=len(failures),
+        failure_details=str(failures) if failures else "none",
+    )
+    log.complete()
+
+    assert not failures, f"Failed to delete objects for prefixes: {failures}"
