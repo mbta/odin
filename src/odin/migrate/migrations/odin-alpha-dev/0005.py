676596e7adcd4def41514f750b1e6ac5f2fb25c8
Max Alexander <w.max.alexander@gmail.com>
2026-08-13 12:59:13 -0400

    Fix paths for migrations (#270)

--- src/odin/migrate/migrations/odin-alpha-dev/0005.py
+++ src/odin/migrate/migrations/odin-alpha-dev/0005.py
@@ -1,6 +1,7 @@
 import os
 
 from odin.utils.aws.s3 import delete_objects, object_exists
+from odin.utils.locations import DATA_SPRINGBOARD
 from odin.utils.locations import CUBIC_ODS_FACT_STATUS
 from odin.utils.logger import ProcessLog
 
@@ -11,7 +12,10 @@ def migration() -> None:
     log = ProcessLog(
         "odin_migration", migration="alpha_dev_0005", target_tables=", ".join(target_tables)
     )
-    target_paths = [os.path.join(CUBIC_ODS_FACT_STATUS, x) + ".json" for x in target_tables]
+    target_paths = [
+        os.path.join(DATA_SPRINGBOARD, CUBIC_ODS_FACT_STATUS, x) + ".json" for x in target_tables
+    ]
+    print(target_paths)
     paths = [x for x in target_paths if object_exists(x)]
     if not all(paths):
         exception = AssertionError("Could not find status tables")
