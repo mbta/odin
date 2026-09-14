e3d019d7518a31656adda1c84f11970ad14996cf
Corey N. Runkel <crunkel@mbta.com>
2026-09-14 10:07:32 -0400

    Update WA160

--- src/odin/generate/data_dictionary/sql/views/wa160.sql
+++ src/odin/generate/data_dictionary/sql/views/wa160.sql
@@ -14,20 +14,13 @@ SELECT
     ut.transit_account_id,
     ut.serial_nbr,
     ut.pass_use_count,
-    coalesce(ut.pass_cost, 0)::double / 100 as pass_cost,
-    coalesce(ut.value_changed, 0)::double / 100 as value_changed,
-    coalesce(ut.booking_prepaid_value, 0)::double / 100 as booking_prepaid_value,
-    coalesce(ut.benefit_value, 0)::double / 100 as benefit_value,
-    coalesce(ut.bankcard_payment_value, 0)::double / 100 as bankcard_payment_value,
-    coalesce(ut.merchant_service_fee, 0)::double / 100 as merchant_service_fee,
-    (
-        coalesce(ut.pass_cost, 0)
-        + coalesce(ut.value_changed, 0)
-        + coalesce(ut.booking_prepaid_value, 0)
-        + coalesce(ut.benefit_value, 0)
-        + coalesce(ut.bankcard_payment_value,0)
-        + coalesce(ut.merchant_service_fee, 0)
-    )::double / 100 as total_use_cost,
+    (CAST(COALESCE(ut.pass_cost, 0) AS DOUBLE) / 100) AS pass_cost,
+    (CAST(COALESCE(ut.value_changed, 0) AS DOUBLE) / 100) AS value_changed,
+    (CAST(COALESCE(ut.booking_prepaid_value, 0) AS DOUBLE) / 100) AS booking_prepaid_value,
+    (CAST(COALESCE(ut.benefit_value, 0) AS DOUBLE) / 100) AS benefit_value,
+    (CAST(COALESCE(ut.bankcard_payment_value, 0) AS DOUBLE) / 100) AS bankcard_payment_value,
+    (CAST(COALESCE(ut.merchant_service_fee, 0) AS DOUBLE) / 100) AS merchant_service_fee,
+    (CAST((((((COALESCE(ut.pass_cost, 0) + COALESCE(ut.value_changed, 0)) + COALESCE(ut.booking_prepaid_value, 0)) + COALESCE(ut.benefit_value, 0)) + COALESCE(ut.bankcard_payment_value, 0)) + COALESCE(ut.merchant_service_fee, 0)) AS DOUBLE) / 100) AS total_use_cost,
     transaction_status_name,
     fpuld.fare_prod_users_list_name,
     trip_price_count,
@@ -52,35 +45,41 @@ SELECT
     tap_id,
     rtd.ride_type_name,
     calculated_fare,
-    coalesce(fpd.rider_class_name, tad.rider_class_name) as rider_class_name,
-    -- coalesce(emd.external_ref, emd.customer_member_id) as external_ref,
-    coalesce(one_account_value, 0) as one_account_value,
-    coalesce(ut.restricted_purse_value, 0) as restricted_purse_value,
-    coalesce(ut.refundable_purse_value, 0) as refundable_purse_value,
-    coalesce(ut.uncollectible_amount, 0)::double / 100 as uncollectible_amount,
+    COALESCE(fpd.rider_class_name, tad.rider_class_name) AS rider_class_name,
+    COALESCE(one_account_value, 0) AS one_account_value,
+    COALESCE(ut.restricted_purse_value, 0) AS restricted_purse_value,
+    COALESCE(ut.refundable_purse_value, 0) AS refundable_purse_value,
+    (CAST(COALESCE(ut.uncollectible_amount, 0) AS DOUBLE) / 100) AS uncollectible_amount,
     tad.is_registered,
-    coalesce(ut.discount_applied, 0)::double / 100 AS discount_amount,
-    coalesce(ut.post_pay_amount, 0)::double / 100 AS post_pay_amount
+    (CAST(COALESCE(ut.discount_applied, 0) AS DOUBLE) / 100) AS discount_amount,
+    (CAST(COALESCE(ut.post_pay_amount, 0) AS DOUBLE) / 100) AS post_pay_amount,
+    CASE
+        WHEN (((ut.TRANSFER_FLAG = 2)
+        AND ((ut.MULTI_RIDE_ID IS NULL)
+        OR (RIDE_COUNT <= 1)))) THEN ('TRANSFER')
+        WHEN (((ut.TRANSFER_FLAG = 2)
+        AND ((ut.MULTI_RIDE_ID IS NOT NULL)
+        OR (RIDE_COUNT > 1)))) THEN ('MULTI-RIDE TRANSFER')
+        WHEN (((ut.TRANSFER_FLAG != 2)
+        AND ((ut.MULTI_RIDE_ID IS NOT NULL)
+        OR (RIDE_COUNT > 1)))) THEN ('MULTI-RIDE')
+        ELSE NULL
+    END AS transfer_or_multiride
 FROM
-    cubic_delta.edw_use_transaction ut
-LEFT JOIN
-    cubic_ods.edw_fare_product_dimension fpd on ut.fare_prod_key = fpd.fare_prod_key
--- LEFT JOIN  -- Disabled because transit_account_id is not unique, duplicating rows.
---     cubic_ods.edw_member_dimension emd on ut.transit_account_id = emd.transit_account_id
-LEFT JOIN
-    cubic_ods.edw_operator_dimension opd on ut.operator_key = opd.operator_key
-LEFT JOIN
-    cubic_ods.edw_card_dimension cardd on ut.card_key = cardd.card_key
-LEFT JOIN
-    cubic_ods.edw_ride_type_dimension rtd on ut.ride_type_key = rtd.ride_type_key
-LEFT JOIN
-    cubic_ods.edw_txn_status_dimension txnsd on ut.txn_status_key = txnsd.txn_status_key
-LEFT JOIN
-    cubic_ods.edw_media_type_dimension mtd on ut.media_type_key = mtd.media_type_key
-LEFT JOIN
-    cubic_ods.edw_transit_account_dimension tad
-        on cardd.transit_account_key = tad.transit_account_key
-LEFT JOIN
-    cubic_ods.edw_fare_prod_users_list_dimension fpuld
-        on fpuld.fare_prod_users_list_key = fpd.fare_prod_users_list_key
-;
+    cubic_delta.edw_use_transaction AS ut
+LEFT JOIN cubic_ods.edw_fare_product_dimension AS fpd ON
+    ((ut.fare_prod_key = fpd.fare_prod_key))
+LEFT JOIN cubic_ods.edw_operator_dimension AS opd ON
+    ((ut.operator_key = opd.operator_key))
+LEFT JOIN cubic_ods.edw_card_dimension AS cardd ON
+    ((ut.card_key = cardd.card_key))
+LEFT JOIN cubic_ods.edw_ride_type_dimension AS rtd ON
+    ((ut.ride_type_key = rtd.ride_type_key))
+LEFT JOIN cubic_ods.edw_txn_status_dimension AS txnsd ON
+    ((ut.txn_status_key = txnsd.txn_status_key))
+LEFT JOIN cubic_ods.edw_media_type_dimension AS mtd ON
+    ((ut.media_type_key = mtd.media_type_key))
+LEFT JOIN cubic_ods.edw_transit_account_dimension AS tad ON
+    ((cardd.transit_account_key = tad.transit_account_key))
+LEFT JOIN cubic_ods.edw_fare_prod_users_list_dimension AS fpuld ON
+    ((fpuld.fare_prod_users_list_key = fpd.fare_prod_users_list_key));
